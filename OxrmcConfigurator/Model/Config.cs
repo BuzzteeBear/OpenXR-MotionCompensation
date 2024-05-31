4cb37374319931fc846d1b3c0fb1f56b75b3a356
BuzzteeBear <buzztee@mailbox.org>
2024-06-01 01:43:35 +0200

    Config wizard prototype

---
+++ OxrmcConfigurator/Model/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OxrmcConfigurator.Model;
+
+public enum Tracker : ushort
+{
+	Error = 0,
+	Controller,
+	Vive,
+	Srs,
+	FlyPt,
+	Yaw
+}
