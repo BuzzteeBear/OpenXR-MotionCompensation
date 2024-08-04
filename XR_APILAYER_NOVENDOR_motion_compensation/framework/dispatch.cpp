712101fd6f6cc7227bf8cea145a6498f1e3f76bb
BuzzteeBear <buzztee@mailbox.org>
2024-08-04 11:36:11 +0200

    Added workaround for Meta V68 firmware bug (xrEnumerateInstanceExtensionProperties)

--- XR_APILAYER_NOVENDOR_motion_compensation/framework/dispatch.cpp
+++ XR_APILAYER_NOVENDOR_motion_compensation/framework/dispatch.cpp
@@ -125,7 +125,8 @@ namespace openxr_api_layer {
                     reinterpret_cast<PFN_xrVoidFunction*>(&xrEnumerateInstanceExtensionProperties)));
 
                 uint32_t extensionsCount = 0;
-                CHECK_XRCMD(xrEnumerateInstanceExtensionProperties(nullptr, 0, &extensionsCount, nullptr));
+                // don't check result, workaround for Meta Quest V68 firmware bug
+                xrEnumerateInstanceExtensionProperties(nullptr, 0, &extensionsCount, nullptr);
                 std::vector<XrExtensionProperties> extensions(extensionsCount, {XR_TYPE_EXTENSION_PROPERTIES});
                 CHECK_XRCMD(xrEnumerateInstanceExtensionProperties(
                     nullptr, extensionsCount, &extensionsCount, extensions.data()));
