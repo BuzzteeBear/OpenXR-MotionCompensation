b940552841af8a1b00fd670258ac14fe250a61fc
Sebastian Veith <buzztee@mailbox.org>
2024-10-14 11:55:45 +0200

    Added dashboard placeholder app

---
+++ OxrmcDashboard/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OxrmcDashboard
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
---
+++ OxrmcDashboardLibrary/DashboardWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace OxrmcDashboard
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private BitmapImage _oxrmcLogo;
+
+		public MainWindow()
+		{
+			_oxrmcLogo = LoadBitmap("oxrmc_grey.png");
+			InitializeComponent();
+		} 
+		private BitmapImage LoadBitmap(string fileName)
+		{
+			BitmapImage bitmap = new BitmapImage();
+			bitmap.BeginInit();
+			bitmap.DecodePixelWidth = 600;
+			bitmap.UriSource = new Uri($"pack://application:,,,/OxrmcDashboardLibrary;component/img/{fileName}");
+			bitmap.EndInit();
+			return bitmap;
+		}
+		private void LoadLogo(object sender, RoutedEventArgs e)
+		{
+				OxrmcLoge.Source = _oxrmcLogo;
+		}
+	}
+}
