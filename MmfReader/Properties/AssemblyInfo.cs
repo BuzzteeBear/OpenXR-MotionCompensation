74b9bb5fbadf9d6d07a20d8b0cfd909fb5edd89d
Sebastian Veith <buzztee@mailbox.org>
2024-10-18 01:19:40 +0200

    Fixed version info for c# projects

--- MmfReader/Properties/AssemblyInfo.cs
+++ MmfReader/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MmfReader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OpenXR-MotionCompensation MmfReader")]
+[assembly: AssemblyDescription("Memory MApped File Reader for OpenXR-MotionCompensation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MmfReader")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("BuzzteeBear")]
+[assembly: AssemblyProduct("OpenXR-MotionCompensation")]
+[assembly: AssemblyCopyright("© Sebastian Veith")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.3.9.0")]
